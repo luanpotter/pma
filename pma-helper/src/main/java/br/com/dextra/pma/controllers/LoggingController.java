9d54bc1db901f164cc90ad6092f78d22fc4a6b29
Luan Nico <luanpotter28@hotmail.com>
2014-10-13 12:14:20 -0300

    Added description while logging

--- pma-helper/src/main/java/br/com/dextra/pma/controllers/LoggingController.java
+++ pma-helper/src/main/java/br/com/dextra/pma/controllers/LoggingController.java
@@ -24,7 +24,7 @@ public class LoggingController extends BaseController {
     private static final String ERROR_MESSAGE = "No name or id is specified, and default task is invalid: '%s'. To change the default task, run options set default-task taskNameOrId";
 
     @Action("here")
-    public CallResult here(@Optional String taskNameOrId) {
+    public CallResult here(@Optional String taskNameOrId, @Optional String description) {
         if (taskNameOrId == null) {
             taskNameOrId = context.o().get(Option.DEFAULT_TASK);
         }
@@ -39,30 +39,30 @@ public class LoggingController extends BaseController {
             return CallResult.ERROR;
         }
 
-        log(new Moment(), task.getId());
+        log(new Moment(), task.getId(), description);
         console.result("Logged successfully.");
         return CallResult.SUCCESS;
     }
 
     @Action("exit")
     public CallResult exit() {
-        log(new Moment(), -1);
+        log(new Moment(), -1, null);
         console.result("Logged successfully.");
         return CallResult.SUCCESS;
     }
 
-    private void log(Moment m, long taskId) {
-        log(context.o().get(Option.LOG_FILE), m, taskId);
+    private void log(Moment m, long taskId, String description) {
+        log(context.o().get(Option.LOG_FILE), m, taskId, description);
 
         String backup = context.o().get(Option.BACKUP_FILE);
         if (backup != null) {
-            log(backup, m, taskId);
+            log(backup, m, taskId, description);
         }
     }
 
-    private void log(String fileName, Moment m, long taskId) {
+    private void log(String fileName, Moment m, long taskId, String description) {
+        String log = m.toString() + (taskId != -1 ? "+" + taskId : "") + (description != null ? "+" + description : "");
         try (PrintWriter p = new PrintWriter(new FileWriter(new File(fileName), true))) {
-            String log = m.toString() + (taskId != -1 ? "+" + taskId : "");
             p.println(log);
         } catch (IOException ex) {
             throw new RuntimeException("Could not log to file!", ex);
@@ -71,8 +71,8 @@ public class LoggingController extends BaseController {
 
     public static void defaultCallables(String name, List<Callable> callables) {
         callables.add(new ActionCall(name + ":here", ":here", "Start counting on default task"));
-        callables.add(new ActionCall(new ActionRef(name + ":here"), new Pattern(":here taskNameOrId", true),
-                "Start counting on taskNameOrId task"));
+        callables.add(new ActionCall(new ActionRef(name + ":here"), new Pattern(":here taskNameOrId", true), "Start counting on taskNameOrId task"));
+        callables.add(new ActionCall(new ActionRef(name + ":here"), new Pattern(":start taskNameOrId :on description", true), "Start counting on taskNameOrId task with description description"));
         callables.add(new ActionCall(name + ":exit", ":exit", "Exit to break"));
     }
 }
--- pma-helper/src/main/java/br/com/dextra/pma/main/PmaSetup.java
+++ pma-helper/src/main/java/br/com/dextra/pma/main/PmaSetup.java
@@ -11,32 +11,34 @@ import br.com.dextra.pma.utils.SimpleObjectAccess;
 
 public class PmaSetup extends FnSetup<PmaContext> {
 
+    private static final String PARSER_FILE_NAME = "parser.dat";
+
     public PmaSetup() {
         super("br.com.dextra.pma.controllers");
     }
 
-    private static final String PARSER_FILE_NAME = "parser.dat";
-
     public static void main(String[] args) {
         create().run(args);
     }
-    
+
     public static Application create() {
         return new PmaSetup().setupApplication(new PmaContext());
     }
-    
+
     @Override
     protected Application createApplication(Console console, PmaContext context) {
         return new FnApplication<PmaContext>(console, context) {
             @Override
             protected void loop(Console console, Context c) {
+                loginAndUpdate(console, context);
+                super.loop(console, context);
+            }
+
+            private void loginAndUpdate(Console console, PmaContext context) {
                 boolean needUpdating = tryLogginInIfNeeded(console);
                 if (needUpdating) {
-                    console.result("Sucessfully logged in. Now wait until your projects and tasks are retrived...");
-                    context.p().update();
-                    console.result("All done! Type help if you need any help.");
+                    update(console, context);
                 }
-                super.loop(console, context);
             }
 
             private boolean tryLogginInIfNeeded(Console console) {
@@ -47,9 +49,15 @@ public class PmaSetup extends FnSetup<PmaContext> {
                     return tryLogginInIfNeeded(console);
                 }
             }
+
+            private void update(Console console, PmaContext context) {
+                console.result("Sucessfully logged in. Now wait until your projects and tasks are retrived...");
+                context.p().update();
+                console.result("All done! Type help if you need any help.");
+            }
         };
     }
-    
+
     @Override
     public Parser getParser() {
         Parser p = readParser();
@@ -62,10 +70,9 @@ public class PmaSetup extends FnSetup<PmaContext> {
     public static Parser readParser() {
         return SimpleObjectAccess.readFrom(PARSER_FILE_NAME);
     }
-    
+
     public static void saveParser(Parser parser) {
         SimpleObjectAccess.saveTo(PARSER_FILE_NAME, parser);
     }
-
 
 }
