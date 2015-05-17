9f36ac6a58d29c7bada5a4c7203d4662305be39a
Luan Nico <luanpotter28@hotmail.com>
2015-05-16 21:47:49 -0300

    small fix (maybe?)

--- src/main/java/br/com/dextra/pma/models/Record.java
+++ src/main/java/br/com/dextra/pma/models/Record.java
@@ -6,9 +6,9 @@ import br.com.dextra.pma.date.Time;
 import br.com.dextra.pma.parser.InvalidFormatException;
 
 /**
- * One record in the file that is being parsed.
- * Several of these will become a day.
- * This is not an appointment, because record times are shifted.
+ * One record in the file that is being parsed. Several of these will become a
+ * day. This is not an appointment, because record times are shifted.
+ * 
  * @author luan
  */
 public class Record {
@@ -27,16 +27,15 @@ public class Record {
 
     public Record(String line, int lineNumber) throws InvalidFormatException {
         String[] parts = line.split("\\+");
-        assertValidLength(lineNumber, parts);
+        assertValidLength(parts, lineNumber);
 
-        this.date = Record.parseDate(parts, lineNumber);
-        this.time = Record.parseTime(parts, lineNumber);
-        this.task = Record.parseTask(parts, lineNumber);
-        this.desc = Record.parseDesc(parts, lineNumber);
-
+        this.date = parseDate(parts, lineNumber);
+        this.time = parseTime(parts, lineNumber);
+        this.task = parseTask(parts, lineNumber);
+        this.desc = parseDesc(parts);
     }
 
-    private static void assertValidLength(int lineNumber, String[] parts) throws InvalidFormatException {
+    private static void assertValidLength(String[] parts, int lineNumber) throws InvalidFormatException {
         if (parts.length <= 1) {
             throw new InvalidFormatException("Each line must have at least 1 '+' signs: date+time[+task[+desc]]; found " + (parts.length - 1), lineNumber);
         }
@@ -66,7 +65,7 @@ public class Record {
         }
     }
 
-    private static String parseDesc(String[] parts, int lineNumber) {
+    private static String parseDesc(String[] parts) {
         String desc = parts.length >= 4 ? parts[3] : ".";
         for (int i = 4; i < parts.length; i++) {
             desc += "+" + parts[i];
