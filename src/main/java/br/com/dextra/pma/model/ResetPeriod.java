bc613de1fcfb7953413030ce890e1db76f2ed10b
Luan Nico <luanpotter28@hotmail.com>
2015-05-29 20:36:42 -0300

    Adding ResetPeriods, and whole Quadrimestre infrastructure

---
+++ src/main/java/br/com/dextra/pma/model/ResetPeriod.java
@@ -0,0 +1,44 @@
+package br.com.dextra.pma.model;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import br.com.dextra.pma.date.Date;
+
+@Getter
+public class ResetPeriod implements Serializable {
+
+    private static final long serialVersionUID = -39166215184122953L;
+
+    private Date start;
+    private Date end;
+
+    private ResetPeriod(Date start, Date end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    private static final int MOUNTH_START = -1, MOUNTH_COUNT = 4;
+
+    public static ResetPeriod findFor(Date date) {
+        int quadrimestre = getQuadrismestre(date);
+        int startingYear = date.getYear(), endingYear = startingYear;
+        int startingMonth = MOUNTH_COUNT * quadrimestre + MOUNTH_START;
+        int endingMonth = startingMonth + MOUNTH_COUNT - 1;
+        if (startingMonth < 0) {
+            startingMonth += 12;
+            startingYear--;
+        }
+        if (endingMonth >= 12) {
+            endingMonth -= 12;
+            endingYear++;
+        }
+        Date firstDate = new Date(startingYear, startingMonth + 1, 1).firstDay();
+        Date lastDate = new Date(endingYear, endingMonth + 1, 1).lastDay();
+        return new ResetPeriod(firstDate, lastDate);
+    }
+
+    public static int getQuadrismestre(Date date) {
+        return (date.getMonth() - MOUNTH_START - 1) / MOUNTH_COUNT;
+    }
+}
---
+++ src/test/java/br/com/dextra/pma/model/ResetPeriodTest.java
@@ -0,0 +1,46 @@
+package br.com.dextra.pma.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.com.dextra.pma.date.Date;
+import static br.com.dextra.pma.model.ResetPeriod.getQuadrismestre;
+
+public class ResetPeriodTest {
+
+    @Test
+    public void findFor() {
+        ResetPeriod period = ResetPeriod.findFor(new Date("2015-01-30"));
+        Assert.assertEquals(new Date("2014-12-01"), period.getStart());
+        Assert.assertEquals(new Date("2015-03-31"), period.getEnd());
+
+        period = ResetPeriod.findFor(new Date("2012-04-13"));
+        Assert.assertEquals(new Date("2012-04-01"), period.getStart());
+        Assert.assertEquals(new Date("2012-07-31"), period.getEnd());
+
+        period = ResetPeriod.findFor(new Date("1996-11-02"));
+        Assert.assertEquals(new Date("1996-08-01"), period.getStart());
+        Assert.assertEquals(new Date("1996-11-30"), period.getEnd());
+
+        period = ResetPeriod.findFor(new Date("2000-12-01"));
+        Assert.assertEquals(new Date("2000-12-01"), period.getStart());
+        Assert.assertEquals(new Date("2001-03-31"), period.getEnd());
+    }
+
+    @Test
+    public void getQuadrimestre() {
+        Assert.assertEquals(0, getQuadrismestre(new Date("2010-01-01")));
+        Assert.assertEquals(0, getQuadrismestre(new Date("2010-02-01")));
+        Assert.assertEquals(0, getQuadrismestre(new Date("2010-03-01")));
+        Assert.assertEquals(1, getQuadrismestre(new Date("2010-04-01")));
+        Assert.assertEquals(1, getQuadrismestre(new Date("2010-05-01")));
+        Assert.assertEquals(1, getQuadrismestre(new Date("2010-06-01")));
+        Assert.assertEquals(1, getQuadrismestre(new Date("2010-07-01")));
+        Assert.assertEquals(2, getQuadrismestre(new Date("2010-08-01")));
+        Assert.assertEquals(2, getQuadrismestre(new Date("2010-09-01")));
+        Assert.assertEquals(2, getQuadrismestre(new Date("2010-10-01")));
+        Assert.assertEquals(2, getQuadrismestre(new Date("2010-11-01")));
+        Assert.assertEquals(3, getQuadrismestre(new Date("2010-12-01")));
+    }
+
+}
